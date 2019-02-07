a28ff5f35028a1a3577c4ffc816e5b1dd63f4f76
Tim Pugh <timpugh@pdx.edu>
2019-02-06 21:53:46 -0800

    Writing to webserver using node.js

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -3,3 +3,13 @@
 var http = require('http'); // do not change this line
 
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+var server = http.createServer(function(req,res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    res.write('lorem ipsum');
+    res.end();
+});
+console.log('server listening on port 8080');
+server.listen(process.env.PORT || 8080);
