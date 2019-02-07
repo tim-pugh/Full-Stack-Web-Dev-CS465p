c7e082bf130840c5f21fcdd5ddbc0b6f41032a5e
Tim Pugh <timpugh@pdx.edu>
2019-02-07 06:29:13 -0800

    Finished DNS exercise

--- node/6-dns.js
+++ node/6-dns.js
@@ -8,3 +8,24 @@ var dns = require('dns'); // do not change this line
 // http://localhost:8080/sniklaus.com should return '216.239.36.21\n216.239.38.21\n216.239.32.21\n216.239.34.21' in plain text (addresses / order might be different)
 
 // http://localhost:8080/error should return 'error' in plain text
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    dns.resolve4(decodeURI(decodeURIComponent(req.url.substr(1))), function (err, addresses) {
+        var str = '';
+        if(err){
+            res.end('error');
+        }
+        for(var i = 0; i < addresses.length; ++i)
+        {
+            str+=addresses[i] + '\n';
+        }
+        res.end(str.trim());
+    });
+
+});
+console.log('server listening on port 8080');
+server.listen(process.env.PORT || 8080);
