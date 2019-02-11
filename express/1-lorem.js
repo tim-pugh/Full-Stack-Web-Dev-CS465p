c9fab9423f380507cd0ddc53833104e9e62c01d7
Tim Pugh <timpugh@pdx.edu>
2019-02-10 17:46:06 -0800

    Exercise one completed

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -3,3 +3,12 @@
 var express = require('express'); // do not change this line
 
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+
+
+var app = express()
+var port = process.env.PORT || 8080
+
+app.get('/lorem', (req, res) => res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
