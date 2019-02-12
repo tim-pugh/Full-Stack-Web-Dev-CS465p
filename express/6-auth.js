db6ea645885629f0a5c4817096dd0989294624d7
Tim Pugh <timpugh@pdx.edu>
2019-02-12 10:39:03 -0800

    Fixed some issues in passport

--- express/6-auth.js
+++ express/6-auth.js
@@ -15,34 +15,59 @@ var strategy = require('passport-http'); // do not change this line
 // http://localhost:8080/test should return 'only accessible when logged in' in plain text if user the user is authenticate, otherwise will prompt for the username and password
 
 var app = express();
+var parser = require('body-parser');
+var cookie = require('cookie-parser');
 var port = process.env.PORT || 8080;
 
+app.use(parser.urlencoded({
+    'extended': true
+}));
 
-passport.use(new strategy.BasicStrategy({ qop: 'auth' },
-  function(username, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      return done(null, user, user.password);
-    });
-  },
-  function(params, done) {
-    // validate nonces as necessary
-    done(null, true);
-  }
+
+app.use(cookie());
+
+app.use(passport.initialize());
+
+passport.use(new strategy.BasicStrategy({
+        qop: 'auth'
+    },
+    function (username, done) {
+        User.findOne({
+            username: username
+        }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false);
+            }
+            return done(null, user, user.password);
+        });
+    },
+    function (params, done) {
+        // validate nonces as necessary
+        done(null, true);
+    }
 ));
 
 
 
-app.get('/',passport.authenticate('basic', { session: false }),
-    function (req, res) {
-
-        res.status(200);
-        res.set({
-            'Content-Type': 'text/plain'
-        });
-        res.send("accessible to everyone");
+app.get('/hello', function (req, res) {
+    res.status(200);
+    res.set({
+        'Content-Type': 'text/plain'
     });
+    res.send('accessible to everyone');
+});
+
+app.get('/test',passport.authenticate('local', { failureRedirect: '/login' }),
+ function (req, res) {
+    res.status(200);
+    res.set({
+        'Content-Type': 'text/plain'
+    });
+    res.send('accessible to everyone');
+});
 
 
 
